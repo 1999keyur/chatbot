9cb2d93bca471af730a8045e08bbfb5a4c61c5b8
Keyur Khakhkhar <61023849+1999keyur@users.noreply.github.com>
2025-02-18 13:53:37 +0530

    style changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,7 @@ import ChatBotWrapper from "./components/ChatBotWrapper/ChatBotWrapper";
 function App() {
   return (
     <>
-      <h1>Welcome to Our Website!</h1>
-      <p>Here’s some content for your site.</p>
+      <h1>Welcome to our Home Page!</h1>
 
       <ChatBotWrapper />
     </>
